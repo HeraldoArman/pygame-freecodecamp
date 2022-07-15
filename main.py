f9149361c9257c4662b2f29b098a2bf90c0043e1
HeraldoArman <88827832+HeraldoArman@users.noreply.github.com>
2022-07-15 17:55:11 +0700

    master

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import pygame
 pygame.init()
 screenX = 800
 screenY = 600
-screen = pygame.display.set_mode((screenX, screenY), pygame.FULLSCREEN)
-
+screen = pygame.display.set_mode((screenX, screenY))
+#, pygame.FULLSCREEN
 pygame.display.set_caption('lolol')
 icon = pygame.image.load("spaceship.png")
 pygame.display.set_icon(icon)
@@ -15,13 +15,21 @@ playerImg = pygame.transform.scale(playerImg, (64, 64))
 playerX = 370
 playerY = 480
 
+EnemyImg = pygame.image.load("alien.png")
+EnemyImg = pygame.transform.scale(EnemyImg, (64, 64))
+EnemyX = 370
+EnemyY = 50
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
 
-PlayerX_change = 0
 
+def enemy(x, y):
+    screen.blit(EnemyImg, (x, y))
+mouse_pos1 = 0
+PlayerX_change = 0
 running = True
+new_mouse_pos = 0
 while running:
     screen.fill((20, 20, 20))
     for event in pygame.event.get():
@@ -32,9 +40,36 @@ while running:
                 running = False
         
         if pygame.mouse.get_pressed():
-            pygame.mouse.set_visible(False)
+            #pygame.mouse.set_visible(False)
             mouse_pos = list(pygame.mouse.get_pos())[0] - screenX/2
-            PlayerX_change = mouse_pos * 0.001
+            
+            if mouse_pos >=screenX/2-50:
+                try:
+                    mouse_pos1 = mouse_pos + new_mouse_pos
+                except NameError:
+                    mouse_pos1 = mouse_pos
+                pygame.mouse.set_pos(screenX/2, screenY/2)
+
+            if mouse_pos <=-screenX/2+50:
+                try:
+                    mouse_pos1 = mouse_pos + new_mouse_pos
+                except NameError:
+                    mouse_pos1 = mouse_pos
+                pygame.mouse.set_pos(screenX/2, screenY/2)
+
+            #try:
+            new_mouse_pos = mouse_pos1 + mouse_pos
+            #except NameError:
+            #    new_mouse_pos = mouse_pos
+            
+            #if mouse_pos <=50:
+            #    mouse_pos1 = mouse_pos
+            #    pygame.mouse.set_pos(screenX/2, screenY/2)
+            
+            
+            print(new_mouse_pos)
+            PlayerX_change = new_mouse_pos * 0.001
+            
 
 
         #if event.type == pygame.KEYDOWN:
@@ -55,8 +90,8 @@ while running:
     if playerX >= 736:
         playerX = 736
 
+    enemy(EnemyX, EnemyY)
     player(playerX, playerY)
-
 
 
 
