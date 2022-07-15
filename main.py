b4232fa01286089db0f5b9d4e7159e06a3f745b8
HeraldoArman <88827832+HeraldoArman@users.noreply.github.com>
2022-07-15 16:21:41 +0700

    master

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pygame
 
 pygame.init()
-
-screen = pygame.display.set_mode((800, 600))
+screenX = 800
+screenY = 600
+screen = pygame.display.set_mode((screenX, screenY), pygame.FULLSCREEN)
 
 pygame.display.set_caption('lolol')
 icon = pygame.image.load("spaceship.png")
@@ -26,19 +27,33 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                PlayerX_change = -0.1
-            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                PlayerX_change = 0.1
+            if event.key == pygame.K_ESCAPE:
+                running = False
+        
+        if pygame.mouse.get_pressed():
+            pygame.mouse.set_visible(False)
+            mouse_pos = list(pygame.mouse.get_pos())[0] - screenX/2
+            PlayerX_change = mouse_pos * 0.001
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_a or event.key == pygame.K_d:
-                PlayerX_change = 0
+
+        #if event.type == pygame.KEYDOWN:
+        #    if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+        #        PlayerX_change = -0.1
+        #    if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+        #        PlayerX_change = 0.1
+#
+        #if event.type == pygame.KEYUP:
+        #    if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_a or event.key == pygame.K_d:
+        #        PlayerX_change = 0
 
     playerX = playerX + PlayerX_change
+
+
+    if playerX <= 0:
+        playerX = 0
+    if playerX >= 736:
+        playerX = 736
 
     player(playerX, playerY)
 
