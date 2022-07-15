7cf363f60bd0b80696576adc2a85f612bd5e86fb
HeraldoArman <88827832+HeraldoArman@users.noreply.github.com>
2022-07-15 15:12:51 +0700

    master

---
+++ main.py
@@ -0,0 +1,49 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+
+pygame.display.set_caption('lolol')
+icon = pygame.image.load("spaceship.png")
+pygame.display.set_icon(icon)
+
+
+playerImg = pygame.image.load("spaceship.png")
+playerImg = pygame.transform.scale(playerImg, (64, 64))
+playerX = 370
+playerY = 480
+
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+
+
+running = True
+while running:
+    screen.fill((20, 20, 20))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            print("lol1")
+        if event.key == pygame.K_RIGHT:
+            print("lol2")
+    if event.type == pygame.KEYUP:
+        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+            print("lepas")
+
+
+    player(playerX, playerY)
+
+
+
+
+
+
+
+    pygame.display.update()
